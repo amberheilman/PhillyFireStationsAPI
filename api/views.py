60353164489d56caf6c9a90cd53ad03072a1ab6b
Amber Heilman <amber.l.heilman@gmail.com>
2015-02-21 15:32:14 -0500

    views.py: add all endpoints

--- api/views.py
+++ api/views.py
@@ -1,4 +1,7 @@
 from api import api
 from api.resources import stations
 
-api.add_resource(stations.Stations, '/')
+api.add_resource(stations.Stations, '/stations')
+api.add_resource(stations.Stations, '/stations/<id>') 
+api.add_resource(incidents.Incidents, '/incidents')
+api.add_resource(incidents.Incidents, '/incidents/<id>')
