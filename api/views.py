e4514bcb8d0a02639aff5c064722338f522802bf
ola-halima <olapade.halima@gmail.com>
2015-02-21 16:54:07 -0500

    incidents.py end point added with dummy data

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from api import api
 from api.resources import stations
+from api.resources import incidents
 
 api.add_resource(stations.Stations, '/stations/', '/stations/<id>')
-#api.add_resource(incidents.Incidents, '/incidents', '/incidents/<id>')
+api.add_resource(incidents.Incidents, '/incidents/', '/incidents/<id>')
