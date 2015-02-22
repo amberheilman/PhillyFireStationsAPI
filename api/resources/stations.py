ef65104f24daae89c5e4cd4ba07827ba2db92ca7
Amber Heilman <amber.l.heilman@gmail.com>
2015-02-22 02:15:43 -0500

    working api

--- api/resources/stations.py
+++ api/resources/stations.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
 from flask.ext import restful
-from flask_restful_swagger import swagger
+from flask_restful.utils import cors
+import json
+from flask import make_response
 from flask import jsonify
 import queries
 import ast
@@ -9,10 +11,9 @@ from api.config import get_config
 
 
 class Stations(restful.Resource):
-    config = get_config()
-
+    @cors.crossdomain(origin='*')
     def get(self):
-        with queries.Session('postgresql://halimaolapade@localhost/philly_fire') as session:
+        with queries.Session('postgresql://fire_read:rad2sz4e@localhost/philly_fire') as session:
             results = session.query("SELECT  s.id,  s.address, s.city,  s.latitude, s.longitude, dt.runs FROM stations as s JOIN ( SELECT   station_id, SUM (runs) AS runs FROM truck_runs  GROUP BY station_id ) as dt ON s.id = dt.station_id")
             return self._transform(results)
 
@@ -29,15 +30,19 @@ class Stations(restful.Resource):
             "total_runs": row['runs']
             }
             response.append(entry)
-        return response
-
+	resp = make_response(json.dumps(response), 200)
+	resp.headers.extend({'Content-Type': 'application/json'})
+	return resp
 
 class Station(restful.Resource):
+    @cors.crossdomain(origin='*')
     def get(self, id):
-        with queries.Session('postgresql://halimaolapade@localhost/philly_fire') as session:
+        with queries.Session('postgresql://fire_read:rad2sz4e@localhost/philly_fire') as session:
             results = session.query("SELECT s.address, s.city, s.id, s.latitude, s.longitude, s.chief, s.service_area, dt.runs AS totalRuns, t.truck_id, t.type, t.runs FROM stations AS s JOIN trucks_with_types_and_runs AS t ON t.station_id = s.id JOIN ( SELECT station_id, SUM(runs) AS runs FROM truck_runs GROUP BY station_id) AS dt ON s.id = dt.station_id WHERE s.id='%s'" % id.upper()) 
             
-            return self._transform(results)
+	    resp = make_response(json.dumps(self._transform(results)), 200)
+  	    resp.headers.extend({'Content-Type': 'application/json'})
+	    return resp
 
 
     def _transform(self, result):
